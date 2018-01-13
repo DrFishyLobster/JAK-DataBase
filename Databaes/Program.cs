442346c3717c722b4ef65e2b2e0119f848d84a8d
WalrusPenguins <faracea@stpaulsschool.org.uk>
2018-01-13 12:12:03 +0000

    Testing

--- Databaes/Program.cs
+++ Databaes/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Databaes
+namespace DatabaseProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
+
+    public partial class Column<T>
+    {
+        public List<T> DataPoints { get; set; }
+        public Column<T> Clone()
+        {
+            Column<T> output = new Column<T>();
+            foreach (var item in DataPoints)
+            {
+                output.DataPoints.Add(item);
+            }
+            return output;
+        }
+
+
+
+    }
+    public partial class Column<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)DataPoints).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)DataPoints).GetEnumerator();
+        }
+
+    }
+
 }
