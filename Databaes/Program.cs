0f633b85da41320f29c0360094c61c0ca029174b
WalrusPenguins <faracea@stpaulsschool.org.uk>
2018-01-13 18:40:32 +0000

    Refresh

--- Databaes/Program.cs
+++ Databaes/Program.cs
@@ -1,43 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-namespace DatabaseProject
+﻿namespace DatabaseProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-
-        }
-    }
-
-    public partial class Column<T>
-    {
-        public List<T> DataPoints { get; set; }
-        public Column<T> Clone()
-        {
-            Column<T> output = new Column<T>();
-            foreach (var item in DataPoints)
-            {
-                output.DataPoints.Add(item);
-            }
-            return output;
-        }
-
-
-
-    }
-    public partial class Column<T> : IEnumerable<T>
-    {
-        public IEnumerator<T> GetEnumerator()
-        {
-            return ((IEnumerable<T>)DataPoints).GetEnumerator();
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ((IEnumerable<T>)DataPoints).GetEnumerator();
-        }
-
     }
 
 }
